6ea42138595cef1cb5569acb2d062590b62d838a
João Marcos Pereira De Souza <112454993+joaomarcospsza@users.noreply.github.com>
2022-10-05 17:26:56 -0300

    Somar
    Atividade 02 da lista

---
+++ Lista De Exercicios 01/Soma.java
@@ -0,0 +1,25 @@
+package lista_01;
+
+import java.util.Scanner;
+
+public class Soma {
+	public static void main(String[] args) {
+	
+		Scanner ler = new Scanner(System.in);
+		
+		int a, b,somar;
+		
+		System.out.println("Informe o primeiro valor: ");
+		a = ler.nextInt();
+		System.out.println("Informe o segundo valor: ");
+		b = ler.nextInt();
+		
+		somar = a + b;
+		
+		System.out.println("A soma de " + a + " + " + b + " fica: " + somar );
+		
+		
+		
+		 
+	}
+}
