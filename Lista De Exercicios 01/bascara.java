04b99610a4185a8ffac95381a3cc83f676714087
João Marcos Pereira De Souza <112454993+joaomarcospsza@users.noreply.github.com>
2022-10-19 13:47:40 -0300

    Update bascara.java

--- Lista De Exercicios 01/bascara.java
+++ Lista De Exercicios 01/bascara.java
@@ -16,29 +16,21 @@ public class bascara {
 		c = ler.nextDouble();
 		
 		if (a == 0)
-		    System.out.println("Estes valores n„o formam uma equaÁ„o de segundo grau ");
+		    System.out.println("Estes valores n√£o formam uma equa√ß√£o de segundo grau; ");
 		    else { 
 				delta = (b * b) - ( 4 * a * c);
 				if (delta < 0)
-				 System.out.println("N„o existe raiz real ");
+				 System.out.println("N√£o existe raiz real ");
 				if (delta == 0) { 
 				System.out.println("Existe uma raiz real");
 				x1 = (- b) / (2 * a);
-		                    //x1Format recebe o valor de x1 e o convert em String
-		                    //assim, posso perguntar se ele contem a fraÁ„o que preciso
-		                    //caso verdadeiro, atribuo o valor de x1 a uma String definida, que seria nesse ex: 1/2
 		                    String x1Format = String.valueOf(x1);
 		                    if (x1Format.contains("0.5")){ 
-		                    //aqui voce faz os outros if, para representar um terÁo, um quarto, um quinto, etc
-		                    //dependendo da necessidade, caso for representaÁıes maiores, teria que mudar um pouco a logica
 		                    x1Format = "1/2";
 		                    System.out.println("Raiz = "+x1Format);
 		                    }
-				//System.out.println("Raiz = "+x1);
-				 }
-		            //------------ fim exemplo
-				if (delta > 0) { 
-				System.out.println("Existem duas raÌzes reais");
+				 }if (delta > 0) { 
+				System.out.println("Existem duas ra√≠zes reais");
 				x1 = (- b + Math.sqrt(delta))/(2 * a);
 				x2 = (- b - Math.sqrt(delta))/( 2 * a);
 				System.out.println("Primeira raiz = " + x1);
